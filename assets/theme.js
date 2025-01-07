c0d1a5b0d8166928444888bbf4704e970ae64fdd
Nicholas Jackson <nicholasjackson5656@gmail.com>
2025-01-06 21:36:01 -0800

    updated the form endpoint to use the proxy server

--- assets/theme.js
+++ assets/theme.js
@@ -1,35 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   const contactForms = Array.from(document.querySelectorAll(".convert-form"));
-  // const kitEndPoint = "https://app.kit.com/forms/7290453/subscriptions";
-  // Exit if no forms are found
+
   if (!contactForms.length) return;
 
-  // Iterate over each form
   contactForms.forEach((contactForm) => {
-    if (contactForm.id == "captchaForm") return false;
-
     contactForm.addEventListener("submit", async (e) => {
       e.preventDefault();
-      // Capture form data
+      debugger;
+
       const formData = new FormData(contactForm);
-
-      // Get the email value, note the correct field name "contact[email]"
       const email = formData.get("contact[email]");
-
-      // ConvertKit API key and form ID
-      const apiKey = "7BkYhDO6DdvssOUZqtC8cw";
-      const formId = "7290453";
 
       try {
         const response = await fetch(
-          `https://api.convertkit.com/v3/forms/${formId}/subscribe`,
+          "https://convert-kit-proxy-server.onrender.com/subscribe",
           {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
             },
             body: JSON.stringify({
-              api_key: apiKey,
               email: email,
             }),
           }
@@ -38,9 +28,9 @@ document.addEventListener("DOMContentLoaded", () => {
         const data = await response.json();
 
         if (response.ok) {
-          console.log("Success:", data);
+          console.log("Subscription successful:", data);
         } else {
-          console.error("Error:", data);
+          console.error("Subscription failed:", data);
         }
       } catch (error) {
         console.error("Error:", error);
